988edeec150620ea9de8fc0926df40b0ed3810a5
yssej <jessyowen48@gmail.com>
2025-06-11 20:10:25 +0300

    feat: create competence entity

---
+++ Competence_service/src/main/java/fr/mbds/itu/competence_service/entities/Competence.java
@@ -0,0 +1,16 @@
+package fr.mbds.itu.competence_service.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Competence {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+
+}
