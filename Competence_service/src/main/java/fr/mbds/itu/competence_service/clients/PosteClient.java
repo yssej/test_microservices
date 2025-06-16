63fc58d6c68949cf6f731542d3bcba8b3a5452cb
yssej <jessyowen48@gmail.com>
2025-06-16 20:27:08 +0300

    feat: add circuit-breaker

--- Competence_service/src/main/java/fr/mbds/itu/competence_service/clients/PosteClient.java
+++ Competence_service/src/main/java/fr/mbds/itu/competence_service/clients/PosteClient.java
@@ -1,6 +1,8 @@
 package fr.mbds.itu.competence_service.clients;
 
+import fr.mbds.itu.competence_service.entities.Competence;
 import fr.mbds.itu.competence_service.models.Poste;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +12,21 @@ import java.util.List;
 @FeignClient(name = "POSTE")
 public interface PosteClient {
     @GetMapping("/poste/{id}")
-    Poste getPoste(@PathVariable Long id);
+    @CircuitBreaker(name = "poste", fallbackMethod = "getDefaultPoste")
+    Poste getPoste(@PathVariable String id);
     @GetMapping("/poste/competence/{id}")
+    @CircuitBreaker(name = "poste", fallbackMethod = "getDefaultListPoste")
     List<Poste> getPosteByCompetenceId(@PathVariable Long id);
+
+    default Poste getDefaultPoste(String id, Exception exception) {
+        Poste poste = new Poste();
+        poste.setId(id);
+        poste.setNom("Not available");
+        poste.setCompetenceIds(null);
+        return poste;
+    }
+
+    default List<Poste> getDefaultListPoste(Exception exception) {
+        return List.of(Poste.builder().nom("Poste service not available").build());
+    }
 }
